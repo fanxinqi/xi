76f714483214c0b422321c539810726af410b4af
lihaili <1216330903@qq.com>
2018-04-16 16:37:34 +0800

    修改配置

--- src/main/java/com/xyb/constants/Constants.java
+++ src/main/java/com/xyb/constants/Constants.java
@@ -1,7 +1,7 @@
 package com.xyb.constants;
 
 public class Constants {
-    public   final static String HEADER_TOKEN="access_token";
+    public   final static String HEADER_TOKEN="access-token";
     public   final static String ADMIN_ROL="admin";
     public   final static String STORE_ROL="store_manager";
 }
